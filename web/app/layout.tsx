160ab5d07c46f040903b63a62d8082dee44bdf83
Anurag Singh <anurags9693@gmail.com>
2025-05-05 17:11:37 +0500

    chore: update package.json to include Solana wallet dependencies and additional visualization libraries

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { WalletConnectionProvider } from "@/components/walletconnection";
+import '@solana/wallet-adapter-react-ui/styles.css'; // 👈 Required for WalletMultiButton styling
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {" "}
+        <WalletConnectionProvider>{children}</WalletConnectionProvider>
       </body>
     </html>
   );
---
+++ web/components/walletconnection.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+export const WalletConnectionProvider = ({ children }: { children: React.ReactNode }) => {
+  const endpoint = clusterApiUrl('devnet');
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
