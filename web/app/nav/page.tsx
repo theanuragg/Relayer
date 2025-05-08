d031a90e18e9fdce81d9fce58e04a94bba79ad41
Anurag Singh <anurags9693@gmail.com>
2025-05-08 09:58:22 +0500

    feat: create NavPage component with Navbar integration

---
+++ web/app/nav/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Navbar from "@/components/ui/navbar"
+
+export default function NavPage() {
+  return (
+    <main className="bg-[#000206] py-20">
+      <Navbar/>
+    </main>
+  )
+}
