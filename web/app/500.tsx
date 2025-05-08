eae9e4bf417f6099d3e4cd12a37d3d222848a55f
Anurag Singh <anurags9693@gmail.com>
2025-05-08 21:21:58 +0500

    feat: add custom error pages for server errors and 404 handling

---
+++ web/app/500.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+// This is the 500 error page for the application. It will be displayed when there is a server error.
+const ServerErrorPage = () => {
+    return <h1>Oops! Something went wrong. (500)</h1>;
+  };
+  
+  export default ServerErrorPage;
+  
