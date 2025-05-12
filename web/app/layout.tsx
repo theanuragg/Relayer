b9aab7884a2b3eef1d668f68c990e78cbebf992a
Anurag Singh <anurags9693@gmail.com>
2025-05-12 23:36:23 +0500

    fix: improve metadata formatting and update descriptions for clarity

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -12,27 +12,41 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "Relayer",
   description:
-    " a visual interface that shows how accounts are connected and interact over time. Designed for devs, users, and analysts, it brings context to raw on-chain data without needing to sift through complex transaction logs.",
-    icons:'/diamond1.png',
-     openGraph: {
+    "A visual interface that shows how accounts are connected and interact over time. Designed for devs, users, and analysts, it brings context to raw on-chain data without needing to sift through complex transaction logs.",
+
+  metadataBase: new URL("https://relayer.theaanurag.xyz"),
+
+   icons: {
+      icon: { url: "/file.svg", sizes: "any" },
+   },
+
+  openGraph: {
     title: "Relayer",
     description:
       "Explore how accounts are visually connected over time. For devs, users, analysts.",
-    url: "https://relayer.theaanurag.xyz/",
+    url: "https://relayer.theaanurag.xyz",
     siteName: "Relayer",
     images: [
       {
-        url: "/metadata.jpg", 
+        url: "/metadata.jpg",
         width: 1200,
         height: 630,
         alt: "Relayer Visual Account Interface",
       },
-    ]
-  }
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Relayer",
+    description:
+      "Explore how accounts are visually connected over time. For devs, users, analysts.",
+    images: ["/metadata.jpg"],
+    creator: "@theaanurag", // optional
+  },
 };
 
 export default function RootLayout({
